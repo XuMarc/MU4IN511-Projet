7bbccfbfb5699f8b6f7d68ac7a96835e76f7fd66
yokshi <yokyann@gmail.com>
2023-11-05 16:26:31 +0100

    Partie 3

--- test.ml
+++ test.ml
@@ -13,16 +13,16 @@ struct
 end
 
 let print_bigint_list (bigint_list : BigInt.integer) : unit =
-  let rec inner (bigint_list : BigInt.integer) : unit =
+  let rec aux (bigint_list : BigInt.integer) : unit =
     match bigint_list with
     | [] -> print_string "[]\n"
     | [b] -> print_string (Int64.to_string b ^ "]\n")
     | head :: tail ->
       print_string (Int64.to_string head ^ "; ");
-      inner tail
+      aux tail
   in
   print_string "[";
-  inner bigint_list
+  aux bigint_list
 ;;
 
   (* Exemple *)
@@ -39,12 +39,12 @@ print_string "\n";;
 
 (* Question 1.2 *)
 let decomposition (x : BigInt.integer) : bool list = 
-  let rec inner (x : BigInt.integer) (acc : bool list) : bool list = 
+  let rec aux (x : BigInt.integer) (acc : bool list) : bool list = 
     match x with 
     | [] -> acc
     | h::t ->
       (* si h = 0L alors on ajoute 64 false à acc *)
-      if h = 0L then inner t (acc @ (List.init 64 (fun _ -> false))) else 
+      if h = 0L then aux t (acc @ (List.init 64 (fun _ -> false))) else 
       (* sinon on ajoute la représentation binaire tranformée de h à acc *)
       let rec convert_to_binary (n : Int64.t) (bits : bool list) : bool list = 
         if n = 0L then bits else 
@@ -56,17 +56,17 @@ let decomposition (x : BigInt.integer) : bool list =
             (* division par 2 <=> decalage vers la droite *)
             convert_to_binary (shift_right_logical n 1) (bit :: bits)
         in 
-          inner t (acc @ (List.rev (convert_to_binary h [])))
-  in inner x [];;
+          aux t (acc @ (List.rev (convert_to_binary h [])))
+  in aux x [];;
 
 
 (* print list and print number of false in the list *)
   let print_list (l : bool list) : unit = 
-    let rec inner (l : bool list) (acc : int) : int = 
+    let rec aux (l : bool list) (acc : int) : int = 
       match l with 
       | [] -> acc
-      | h::t -> inner t (acc + (if h then 0 else 1))
-    in List.iter (fun b -> print_string (if b then "1" else "0")) l; print_string "\nNumber of 0 : "; print_int (inner l 0);;
+      | h::t -> aux t (acc + (if h then 0 else 1))
+    in List.iter (fun b -> print_string (if b then "1" else "0")) l; print_string "\nNumber of 0 : "; print_int (aux l 0);;
   
     let rec print_bool_list bool_list =
       match bool_list with
@@ -95,15 +95,13 @@ print_string "\n";;
 (* liste tronquée, n premiers elems *)
 let completion (l : bool list) (n : int) : bool list =
   if n <= 0 then [] else
-  let rec inner (l : bool list) (n : int) (acc : bool list) : bool list = 
+  let rec aux (l : bool list) (n : int) (acc : bool list) : bool list = 
     match l with 
     | [] -> acc@(List.init n (fun _ -> false))
     | h::t -> 
       if n = 0 then acc else
-      inner t (n-1) (acc @ [h])
-  in inner l n [];;
-
-  
+      aux t (n-1) (acc @ [h])
+  in aux l n [];;
 
 (* Exemple *)
 print_string "\nQuestion 1.3 test\n";;
@@ -123,12 +121,15 @@ let rec pow (n : Int64.t) (e : int) : Int64.t =
 
 (* Question 1.4 *)
 let composition (l : bool list) : BigInt.integer =
-  (* parcourt la liste des bools *)
-  let rec inner (l : bool list) (acc : BigInt.integer) (pos : int): BigInt.integer = 
+  match l with     
+  | [false] -> [0L]
+  | _ ->
+  (* parcourt la liste des bools avec pos la puissance de 2 à rajouter td 2^pos *)
+  let rec aux (l : bool list) (acc : BigInt.integer) (pos : int): BigInt.integer = 
     match l with 
     | [] -> acc
     | h::t -> 
-      (* permet d'ajouter la puissance de 2 adequate a l'accumulateur *)
+      (* appel de add si on rencontre un "true" *)
       let rec add (n : BigInt.integer) (acc : BigInt.integer) (pos : int) : BigInt.integer = 
         match n with 
         | [] -> BigInt.inserer_queue (pow 2L pos) acc
@@ -139,15 +140,14 @@ let composition (l : bool list) : BigInt.integer =
           else
             BigInt.inserer_queue (Int64.add h (pow 2L pos)) acc
       in 
-      if h then inner t (add acc [] pos) (pos+1)
-      
+      if h then aux t (add acc [] pos) (pos+1)
       else 
-        (* si le nb delement analysé  *)
-        if pos = 63 then inner t (BigInt.inserer_queue 0L acc) 0  else
-          inner t acc (pos+1) 
-    in 
-    inner l [] 0 
-    ;;
+        (* si on atteint la fin d'un bloc de 64 bits. *)        
+        if pos = 63 then aux t (BigInt.inserer_queue 0L acc) 0  else
+          aux t acc (pos+1) 
+  in 
+  aux l [] 0 
+;;
 
 
 (* Exemple *)
@@ -171,18 +171,8 @@ print_bigint_list result;;
 print_string "\n";;
 
 (* Question 1.5 *)
-(* Je suis pas sur si c'est ce qu'on veut ? surtout la signature mhmm *)
 let table (x : int) (n : int) : bool list =
   completion (decomposition [Int64.of_int x]) n
-;;
-
-(* avec des Int64.t *)
-let rec table2 (x : BigInt.integer) (n : BigInt.integer) : bool list =
-  let a = decomposition x in
-  (* completion a (Int64.to_int n) *)
-  match n with 
-  | [] -> []
-  | h::t -> completion a (Int64.to_int h) @ table2 x t
 ;;
 
 (* d'apres les arbres de construction  *)
@@ -192,30 +182,36 @@ print_string "\nQuestion 1.5 test\n";;
 let result = table 38 10;;
 print_list result;;
 print_string "\n";;
-print_string "\navec les int64\n";;
-let result = table2 [38L] [10L];;
-print_list result;;
-print_string "\n";;
 
-(* Question 1.6 n est un entier naturel parce que je vois pas comment divviser une liste de int64 avec 64*)
-let genAlea ( n : int) : BigInt.integer=
+(* générant un grand entier aléatoire de n bits au maximum *)
+let genAlea (n : int) : BigInt.integer =
   (* pour avoir des valeurs differentes a l'appel de Random.int64 *)
   Random.self_init ();
   (* genere la liste d'int64.t *)
-  let rec inner (n : int) (acc : BigInt.integer) : BigInt.integer = 
-    if n>63 then inner (n-64) (BigInt.inserer_queue (Random.int64 Int64.max_int) acc) else
-      if n<64 then BigInt.inserer_queue (Random.int64 (pow 2L n)) acc else
-        acc
-  in inner n []
+  let rec aux (n : int) (acc : BigInt.integer) : BigInt.integer = 
+    match n with
+    | n when n <= 0 -> acc
+    | n when n < 64 -> BigInt.inserer_queue (Random.int64 (pow 2L n)) acc
+    | n -> aux (n-64) (BigInt.inserer_queue (Random.int64 Int64.max_int) acc)
+  in
+  match n with
+  | n when n < 64 -> [Random.int64 (pow 2L n)]
+  | n -> aux n []
 ;;
 
-let rec genAleaBigInt ( n : BigInt.integer ) : BigInt.integer =
-  match n with 
-  [] -> []
-  | h::t -> 
-    genAlea (Int64.to_int h) @ genAleaBigInt t
+(* générant un grand entier aléatoire de n bits au maximum *)
+let genAlea (n : int) : BigInt.integer =
+  (* pour avoir des valeurs differentes a l'appel de Random.int64 *)
+  Random.self_init (); 
+  (* genere la liste d'int64.t *)
+  let rec aux (n : int) (acc : BigInt.integer) : BigInt.integer = 
+    match n with
+    | n when n <= 0 -> acc 
+    | n when n < 64 -> BigInt.inserer_queue (Random.int64 (pow 2L n)) acc 
+    | n -> aux (n-64) (BigInt.inserer_queue (Random.int64 Int64.max_int) acc) 
+  in
+  aux n [] 
 ;;
-
 
 (* Exemple *)
 print_string "\nQuestion 1.6 test\n";;
@@ -240,7 +236,7 @@ type decision_tree =
 (* Exercice 2.8 *)
 let cons_arbre (tab : bool list) : decision_tree =
   (* tab : liste, pos : profondeur, taille :  *)
-  let rec inner (tab : bool list) (pos : int): decision_tree =
+  let rec aux (tab : bool list) (pos : int): decision_tree =
     match tab with
     | [] -> Leaf false
     | [b] -> Leaf b
@@ -252,25 +248,25 @@ let cons_arbre (tab : bool list) : decision_tree =
         Node (pos, g, d)
       else
         (* g contient la premiere moitié de la liste et d lautre *)
-        let g = inner (List.init (List.length tab/2) (fun i -> List.nth tab i)) (pos+1) 
-        and d = inner (List.init (List.length tab /2) (fun i -> List.nth tab (i + List.length tab/2))) (pos+1)  in
+        let g = aux (List.init (List.length tab/2) (fun i -> List.nth tab i)) (pos+1) 
+        and d = aux (List.init (List.length tab /2) (fun i -> List.nth tab (i + List.length tab/2))) (pos+1)  in
         Node (pos, g, d)
   in
-  inner tab 1 
+  aux tab 1 
 ;;
 
 let print_tree (dt : decision_tree ) : unit =
-  let rec inner (dt : decision_tree) : unit =
+  let rec aux (dt : decision_tree) : unit =
     match dt with
     | Leaf b -> print_string (string_of_bool b)
     | Node (pos, left, right) ->
       print_string ("(" ^ string_of_int pos ^ ", ");
-      inner left;
+      aux left;
       print_string ", ";
-      inner right;
+      aux right;
       print_string ")"
   in
-  inner dt
+  aux dt
 ;;
 
 
@@ -298,13 +294,13 @@ print_string "\n";;
 
 (* liste les feuilles d'un sous noeud de l'arbre *)
 let liste_feuilles (noeud : decision_tree) : bool list =
-  let rec inner (noeud : decision_tree) (acc : bool list) : bool list =
+  let rec aux (noeud : decision_tree) (acc : bool list) : bool list =
     match noeud with
     | Leaf b -> b::acc
     | Node (pos, left, right) ->
-      inner left acc @ inner right acc
+      aux left acc @ aux right acc
   in
-  inner noeud []
+  aux noeud []
 ;;
 
 (* Exemple *)
@@ -323,47 +319,34 @@ print_list result;;
 print_string "\n";;
 
 (* Exercice 3 : Compression de l'arbre de décision et ZDD *)
+type listeDejaVus = (BigInt.integer * decision_tree ) list
 
-type listeDejaVus = (BigInt.integer * decision_tree ref) list
-
-(* Question 3.1 *)
-(* 
-let compressionParListe ( gt : decision_tree) (ldv : listeDejaVus) : decision_tree * listeDejaVus =
-  match gt with 
-  | Leaf b -> (Leaf b, ldv)
-  | x-> let l = liste_feuilles x in 
-  (* deuxieme moitié de l *)
-  let lg = List.init (List.length l /2) (fun i -> List.nth l (i + List.length l/2)) in
-  (* premiere moitié de l *)
-  let ld = List.init (List.length l /2) (fun i -> List.nth l i) in
-  (* on rajoute le noeud dans les neouds visité *)
-  let ldv = (lg, x)::ldv in
-  (* si la deuxieme moitié ne contient que des false on remplace le pointeur vers N vers un pointeur vers l'enfant gauche de N*)
-  if List.for_all (fun x -> x=false) ld then 
-
-;; *)
-(*
-let rec compressionParListe (arbre : decision_tree) (listeDejaVus : listeDejaVus) : decision_tree =
-  match arbre with
-  | Leaf b -> Leaf b
-  | Node (pos, left, right) ->
-    let liste_feuilles_N = liste_feuilles arbre in
-    let moitie_gauche, moitie_droite = List.split_at (List.length liste_feuilles_N / 2) liste_feuilles_N in
-    if List.for_all (fun b -> not b) moitie_droite then
-      compressionParListe left listeDejaVus
-    else
-      let n = composition moitie_feuilles_N in
-      match List.assoc_opt n listeDejaVus with
-      | Some compressed_tree_ref -> compressionParListe !compressed_tree_ref listeDejaVus
-      | None ->
-        let new_compressed_tree_ref = ref arbre in
-        let new_listeDejaVus = (n, new_compressed_tree_ref) :: listeDejaVus in
-        compressionParListe (Node (pos, compressionParListe left new_listeDejaVus, compressionParListe right new_listeDejaVus)) new_listeDejaVus
+let compressionParListe (dt : decision_tree) (ldv : listeDejaVus) : decision_tree * listeDejaVus =
+  let rec aux (dt : decision_tree) (ldv : listeDejaVus) : decision_tree * listeDejaVus =
+    (* Calcul du nombre de feuilles dans l'arbre *)
+    let n = composition (liste_feuilles dt) in
+    (* Si l'arbre a déjà été visité, on renvoie le noeud correspondant *)
+    match List.assoc_opt n ldv with
+    | Some d -> (d, ldv)
+    (* Sinon, on continue la compression *)
+    | None ->
+      match dt with
+      (* Si le noeud est une feuille, on renvoie le noeud tel quel *)
+      | Leaf b -> (dt, (n, dt)::ldv)
+      | Node (pos, left, right) ->
+        (* On compresse les fils et on donne au fils droit la ldv resultante de l'appel a la compression du fils gauche *)
+        let (dl, ld) = aux left ldv in
+        let (dr, lr) = aux right ld in
+        let new_node =
+          match dr with
+          (* REGLE-Z : Si le fils droit est une feuille false, on compresse le noeud avec le fils gauche *)
+          | Leaf false -> Node (pos, dl, dl)
+          | _ -> Node (pos, dl, dr)
+        in
+        (new_node, (n, new_node)::lr)
+  in
+  aux dt ldv
 ;;
-
-
-
-
 (* Exemple *)
 print_string "\nQuestion 3.1 test\n";;
 let mtable = completion (decomposition [25899L]) 16;;
@@ -374,112 +357,105 @@ let result = cons_arbre mtable;;
 print_string "Result : ";;
 print_tree result;;
 print_string "\n";;
-let (result, ldv) = compressionParListe result [];;
+let (t, ldv) = compressionParListe result [];;
 print_string "Result : ";;
-print_tree result;;
+print_tree t;;
 print_string "\n";;
 print_string "Liste de noeuds visités : ";;
 (* print_bigint_list (List.map (fun (x, y) -> x) ldv);; *)
 print_string "\n";;
 
-*)
-
-(* Question 3.2 *)
-
-
-
-(* Question 3.12 *)
-
-
 open Printf
 
-(*
+let dot (tree : decision_tree) (ldv : listeDejaVus) (filename: string)= 
+  let rec write_dot_tree_edges (tree : decision_tree) (chan : out_channel) (parent_id : string option) (is_right_child : bool) =
+    (* Calcul du nombre de feuilles dans l'arbre *)
+    let n = (composition (liste_feuilles tree)) in
+    (* Si l'arbre n'a pas été compressé *)
+    if ldv = [] then
+      match tree with
+      | Leaf b ->
+        let node_id = string_of_int (Random.int 100000) in
+        let shape = "box" in
+        let color = if b then "green" else "red" in
+        fprintf chan "%s [label=\"%b\", shape=%s, color=%s];\n" node_id b shape color;
+        (* Si le noeud a un parent, on écrit le lien entre le parent et le noeud *)
+        begin
+          match parent_id with
+          | Some p_id ->
+            let style = if is_right_child then "solid" else "dashed" in
+            fprintf chan "%s -> %s [style=%s];\n" p_id node_id style
+          | None -> ()
+        end
+      | Node (pos, left, right) ->
+        let node_id = string_of_int (Random.int 100000) in
+        fprintf chan "%s [label=\"%d\", shape=ellipse];\n" node_id pos;
+        begin
+          match parent_id with
+          | Some p_id ->
+            let style = if is_right_child then "solid" else "dashed" in
+            fprintf chan "%s -> %s [style=%s];\n" p_id node_id style
+          | None -> ()
+        end;
+        (* On continue la récursion sur les fils gauche et droit *)
+        write_dot_tree_edges left chan (Some node_id) false;
+        write_dot_tree_edges right chan (Some node_id) true
+    (* Si l'arbre a été compressé *)
+    else
+      match tree with 
+      | Leaf b ->
+        let node_id = (Int64.to_string (List.hd n)) in
+        let shape = "box" in
+        let color = if b then "green" else "red" in
+        fprintf chan "%s [label=\"%b\", shape=%s, color=%s];\n" node_id b shape color;
+        begin
+          match parent_id with
+          | Some p_id ->
+            let style = if is_right_child then "solid" else "dashed" in
+            fprintf chan "%s -> %s [style=%s];\n" p_id node_id style
+          | None -> ()
+        end
+      | Node (pos, left, right) ->
+        let node_id = (Int64.to_string (List.hd n))^"."^(string_of_int pos) in
+        fprintf chan "%s [label=\"%d\", shape=ellipse];\n" node_id pos;
+        begin
+          match parent_id with
+          | Some p_id ->
+            let style = if is_right_child then "solid" else "dashed" in
+            fprintf chan "%s -> %s [style=%s];\n" p_id node_id style
+          | None -> ()
+        end;
+        write_dot_tree_edges left chan (Some node_id) false;
+        write_dot_tree_edges right chan (Some node_id) true
 
-let rec write_dot_tree_edges tree chan parent_id =
-  match tree with
-  | Leaf b ->
-    let node_id = string_of_int (Random.int 100000) in
-    fprintf chan "%s [label=\"%b\", shape=box];\n" node_id b;
-    begin
-      match parent_id with
-      | Some p_id -> fprintf chan "%s -> %s;\n" p_id node_id
-      | None -> ()
-    end
-  | Node (feature, left, right) ->
-    let node_id = string_of_int (Random.int 100000) in
-    fprintf chan "%s [label=\"%d\"];\n" node_id feature;
-    begin
-      match parent_id with
-      | Some p_id -> fprintf chan "%s -> %s;\n" p_id node_id
-      | None -> ()
-    end;
-    write_dot_tree_edges left chan (Some node_id);
-    write_dot_tree_edges right chan (Some node_id)
-*)
+  in 
 
-let rec write_dot_tree_edges (tree : decision_tree) (chan : out_channel) (parent_id : string option) (is_right_child : bool) =
-  match tree with
-  | Leaf b ->
-    let node_id = string_of_int (Random.int 100000) in
-    let shape = "box" in
-    let color = if b then "green" else "red" in
-    fprintf chan "%s [label=\"%b\", shape=%s, color=%s];\n" node_id b shape color;
-    begin
-      match parent_id with
-      | Some p_id ->
-        let style = if is_right_child then "solid" else "dashed" in
-        fprintf chan "%s -> %s [style=%s];\n" p_id node_id style
-      | None -> ()
-    end
-  | Node (feature, left, right) ->
-    let node_id = string_of_int (Random.int 100000) in
-    fprintf chan "%s [label=\"%d\", shape=ellipse];\n" node_id feature;
-    begin
-      match parent_id with
-      | Some p_id ->
-        let style = if is_right_child then "solid" else "dashed" in
-        fprintf chan "%s -> %s [style=%s];\n" p_id node_id style
-      | None -> ()
-    end;
-    write_dot_tree_edges left chan (Some node_id) false;
-    write_dot_tree_edges right chan (Some node_id) true
-
-let create_dot_tree (tree : decision_tree) (filename : string) =
   let chan = open_out filename in
-  fprintf chan "digraph Tree {\n";
-  write_dot_tree_edges tree chan None false;
-  fprintf chan "}\n";
+  (* Écriture de l'en-tête du fichier .dot *)
+  fprintf chan "strict digraph Tree {\n";
+  write_dot_tree_edges tree chan None false ;
+  fprintf chan "}\n"; 
   close_out chan
 ;;
 
-
-
+  
+  (* Exemple *)
+print_string "\nQuestion 3.2 test\n";;
 let mtable = completion (decomposition [25899L]) 16;;
 print_string "Table : ";;
 print_list mtable;;
 print_string "\n";;
-let result = cons_arbre mtable
-in create_dot_tree result "./tree.dot"
+let result = cons_arbre mtable;;
+print_string "Result : ";;
+print_tree result;;
+print_string "\n";;
+dot result [] "./dotFiles/tree.dot" 
+(* create jpg *)
+let _ = Sys.command "dot -Tjpg ./dotFiles/tree.dot -o ./jpgFiles/tree.jpg"
+let (tree, ldv) = compressionParListe result [];;
+dot tree ldv "./dotFiles/tree_compressed.dot" 
 
-let rec traverse_postfix tree =
-  match tree with
-  | Leaf b -> [b]
-  | Node (_, left, right) ->
-      let left_result = traverse_postfix left in
-      let right_result = traverse_postfix right in
-      left_result @ right_result
+(* create jpg *)
+let _ = Sys.command "dot -Tjpg ./dotFiles/tree_compressed.dot -o ./jpgFiles/tree_compressed.jpg"
 
-let example_tree =
-  Node (1, Leaf true, Node (2, Leaf false, Leaf true))
 
-let result = traverse_postfix example_tree
-
-type listeDejaVus = (BigInt.integer * decision_tree ref) list
-
-type decision_tree =
-  | Leaf of bool
-  | Node of int * decision_tree * decision_tree
-
-type arbreDejaVus = 
-  | Leaf of bool 
-  | Node of 
